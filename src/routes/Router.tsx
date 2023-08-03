08a0f4dba5414f16ab5fa51d40832d65b7b607e2
KarineAlsa <saremana@hotmail.com>
2023-08-03 17:14:23 -0600

    router fixed

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,18 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import { LoginPage } from "../pages/LoginPage"
 import { PrivateRoutes } from "./PrivateRoutes"
-import {useState, useEffect} from "react"
+import {useState} from "react"
 
 export const Router = () => {
     const [auth, setAuth] = useState<boolean>(false);
 
-    useEffect(() =>{
+    useState(() =>{
     const token = localStorage.getItem('auth');
     if(token?.length === 32){
         setAuth(true);
     }
-    },[])
-
+    })
     return (
         <BrowserRouter>
             <Routes>
