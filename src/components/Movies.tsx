fce3ce59f789837169f71bd88734149134dab195
Karine Alcázar <67878261+KarineAlsa@users.noreply.github.com>
2023-08-04 17:47:34 -0600

    Update Movies.tsx

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -6,6 +6,8 @@ interface Movie {
   title: string;
   overview: string;
   posters: string;
+  date:string;
+  genre: string;
   stars: number;
 }
 
@@ -14,7 +16,7 @@ interface MoviesProps {
 }
 
 const Movies: React.FC<MoviesProps> = ({ movies }) => {
-  
+
   return (
     <div className="mx-auto h-full px-16">
       <div className="grid gap-6 grid-cols-5 place-items-center">
@@ -27,7 +29,9 @@ const Movies: React.FC<MoviesProps> = ({ movies }) => {
                 alt=""
               />
               <div className="absolute h-full w-full bg-[#5241eaa7] flex flex-col items-center pt-8 px-6 -top-0 group-hover:top-0 opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out text-white cursor-pointer">
-                <h2 className="text-lg text-center">{movie.title}</h2>
+                <h2 className="text-lg text-center">{movie.title} </h2>
+                <h3 className="text-xs text-center">{movie.date} </h3>
+                <h4 className="text-xs text-center ">{movie.genre} </h4>
                 <p className="text-xs line-clamp-5 mb-10 ">{movie.overview}</p>
                 <Stars stars={movie.stars} />
               </div>
