acbbfdf4daae3c97751db17391c0cb1659cb2a61
KarineAlsa <saremana@hotmail.com>
2023-08-04 13:41:59 -0600

    routes test

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -23,10 +23,10 @@ export const Router = () => {
                     auth 
                         ? <Route path="/*" element={<PrivateRoutes />} />
                             
-                        : <Route path="/" element={<LoginPage />} />
+                        : <Route path="/login" element={<LoginPage />} />
                 }
                 
-                <Route path='*' element={<Navigate to='/' replace />} />
+                <Route path='*' element={<Navigate to='/login' replace />} />
             </Routes>
         </BrowserRouter>
     )
