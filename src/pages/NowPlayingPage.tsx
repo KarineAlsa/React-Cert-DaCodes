3dcfebda60feb73b66d1182c36487d2ce21f6154
Karine Alcázar <67878261+KarineAlsa@users.noreply.github.com>
2023-08-04 16:21:58 -0600

    Update NowPlayingPage.tsx

--- src/pages/NowPlayingPage.tsx
+++ src/pages/NowPlayingPage.tsx
@@ -106,7 +106,7 @@ const handlePrevious = (e: React.MouseEvent<HTMLAnchorElement>) => {
         <h2 className="text-white text-3xl mb-8 mt-10">
           Now Playing
         </h2>
-        <div className="h-screen mb-44">
+        <div className="h-full mb-44">
         <Movies movies={movies} />
         </div>
         <div className="w-full p-16">
